f65e2021ffe1c092b5ae6986934557853a573e44
lbryant-sss <brianmungai350@gmail.com>
2024-03-18 21:09:59 +0300

    HttpResponse Done

--- logger/logger/settings.py
+++ logger/logger/settings.py
@@ -11,11 +11,14 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+TEMPLATE_DIRS = [
+    BASE_DIR / 'templates',
+    'logpages',
+]
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
